f54b5911ee121f47ddcdc294f66aac4b0592f08b
ivy89 <ivana.zecevic89@gmail.com>
2015-02-11 21:13:59 +0000

    Adding solution/project
    Ading solution/project

---
+++ TestApplication/TestApplication/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApplication.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
